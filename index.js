c83ce3d2884c7d47c53d38d0fd84146ef93ae6f5
egislook <noneedsystem@gmail.com>
2020-12-30 08:33:05 +0700

    initial

---
+++ index.js
@@ -0,0 +1,76 @@
+const fetchier = require('fetchier')
+const qs = require('qs')
+
+module.exports.success = success
+module.exports.fail = fail
+module.exports.result = result
+module.exports.parse = parse
+module.exports.act = act
+module.exports.fetch = fetchier.fetch
+
+const { URL, GQL_URL, TOKEN, HEADERS } = require(process.cwd() + '/config.js') || {}
+
+function act(action, request = {}) {
+  const { token, path = '', headers, ...rest } = request
+  const query = request.query ? ('?' + qs.stringify(query)) : ''
+
+  return fetchier[action]({
+    url: (request.url || URL) + path + query,
+    token: token || TOKEN,
+    headers: headers || HEADERS,
+    ...rest
+  })
+}
+
+function result(statusCode, data, error, extra = {}){
+
+  const details = {
+    statusCode,
+    version: 'v' + process.env && process.env.npm_package_version,
+  }
+
+  if(!!error) details.message = typeof error === 'object' ? (error.message || error) : error
+  if(!!error && !!error.status) details.status = typeof error === 'object' ? (error.status || null) : null
+
+  return {
+    statusCode,
+    headers: {
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Credentials' : true,
+      'Access-Control-Allow-Origin': '*'
+    },
+    body: JSON.stringify({ ...details, data, ...extra })
+  }
+}
+
+function success(body, message, extra){
+  return result(200, body, message, extra);
+}
+
+function fail(error, body, code = 500){
+
+  if(typeof error !== 'string'){
+    code  = error.statusCode && error.statusCode || code
+    body  = error.data
+    error = error
+  }
+
+  return result(code, body, error)
+}
+
+function parse(event) {
+
+  let body
+  try { body = JSON.parse(event.body) || {} } catch (error) { body = event.body }
+
+  const authorization = event.headers && (event.headers.authorization || event.headers.Authorization)
+
+  return {
+    body,
+    token: authorization && (authorization && authorization.replace(/bearer(\s{1,})?/i, '')),
+    headers: event.headers,
+    id: event.pathParameters && event.pathParameters.id,
+    params: event.pathParameters,
+    query: event.queryStringParameters
+  }
+}
